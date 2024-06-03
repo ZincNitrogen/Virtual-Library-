4d2d7619341dd6dd2a0b48ca71e30a9abae83ec3
Ziyad Noorhassan <67880504+thEchroniCamateuR@users.noreply.github.com>
2024-06-03 16:30:11 -0400

    Added a modal form dialog box to take user input and save return values as objects in an array.
    Could not figure out how to get "dialog.returnValue" to work, so I am using the value of the submit button instead. Seems to work fine. I also have a vision of appending this object to an array and a set.. The array, allBooks, will be the data structure behind the client-side search function. The set, will temporarily hold the book object at the time of creation so as to parse it to a function that injects a div for a card-like display.
    
    TODO: create set for temporary object storage and create function that creates divs for a card-like display that will accept the object stored in the set as a parameter.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const addButton = document.querySelector(".addButton");
+const submitButton = document.querySelector(".submitButton");
 const closeButton = document.querySelector(".closeButton");
 const clearButton = document.querySelector(".clearButton");
 const dialog = document.querySelector("dialog");
+let titleValue = document.querySelector("#title");
+let authorValue =  document.querySelector("#author");
+let startDateValue =  document.querySelector("#date-started");
+let endDateValue =  document.querySelector("#date-ended");
+let pagesReadValue =  document.querySelector("#pages-read");
+let allBooks = [];
+
+
+
+
+function createBookObject(title = null, author=null, startDate=null, endDate=null, pagesRead=null) {
+    this.title = title;
+    this.author = author;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.pagesRead = pagesRead;
+}
 
 
 
@@ -16,8 +34,35 @@ addButton.addEventListener('click', (e) => {
     dialog.showModal();
 
 
-})
+});
 
 closeButton.addEventListener('click', (e) => {
     dialog.close();
-})
+});
+
+submitButton.addEventListener("click", (e) => {
+
+    /*get individual field data
+    parse data into an object
+    give e.value the value of that object.*/
+
+
+    let book = new createBookObject(titleValue.value, authorValue.value, startDateValue.value, endDateValue.value, pagesReadValue.value)
+    allBooks.push(book);
+    e.value = book;
+    console.log(e.value);
+    titleValue.value = null;
+    authorValue.value = null;
+    startDateValue.value = null ;
+    endDateValue.value = null;
+    pagesReadValue.value = null;
+
+    console.log(allBooks);
+    e.preventDefault();
+    dialog.close();
+
+});
+
+//NEXT STEP AFTER LUNCH: FIGURE OUT HOW TO PARSE INFORMATION OUT OF DIALOG ELEMENT
+
+//make it so clicking out of the dialog exits the dialog without saving or updating info
